89c2b704a1b24b1422f2ea2b2a4656d1bb676cdc
Scott Kuchinski <scottkuchinski@gmail.com>
2018-11-15 12:27:30 -0500

    env file built, api config built

--- src/context.js
+++ src/context.js
@@ -1,22 +1,18 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 
 const Context = React.createContext();
 
 export class Provider extends Component {
     state = {
-        track_list: [
-            {
-                track: {
-                    track_name: 'abc'
-                }
-            },
-            {
-                track: {
-                    track_name: '123'
-                }
-            }
-        ],
+        track_list: [],
         heading: 'Top 10 tracks'
+    };
+
+    componentDidMount() {
+        axios.get(`https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${process.env.REACT_APP_MM_KEY}`)
+            .then(res => console.log(res.data))
+            .catch(err => console.log(err));
     }
 
   render() {
