8afc58a94343ed3116fd1ab9e0afa25e350be57c
Scott Kuchinski <scottkuchinski@gmail.com>
2018-11-16 11:33:39 -0500

    lyrics component built

--- src/components/tracks/Lyrics.js
+++ src/components/tracks/Lyrics.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom'
 import Spinner from '../layout/Spinner';
+import Moment from 'react-moment';
 
 class Lyrics extends Component {
     state = {
@@ -31,7 +33,34 @@ class Lyrics extends Component {
             ) {
             return <Spinner />
         } else {
-            return <h1>Data Returned</h1>
+            return (
+                <React.Fragment>
+                    <Link to="/" className="btn btn-dark btn-sm mb-4">Go Back</Link>
+                    <div className="card">
+                        <h5 className="card-header">
+                            {track.track_name} by <span className="text-secondary">{track.artist_name}</span>
+                        </h5>
+                        <div className="cardbody">
+                            <p className="card-text">{lyrics.lyrics_body}</p>
+                        </div>
+                    </div>
+
+                    <ul className="list-group mt-3">
+                        <li className="list-group-item">
+                            <strong>Album ID</strong>: {track.album_id}
+                        </li>
+                        <li className="list-group-item">
+                            <strong>Song Genre</strong>: {track.primary_genres.music_genre_list[0].music_genre.music_genre_name}
+                        </li>
+                        <li className="list-group-item">
+                            <strong>Explicit Words</strong>: {track.explicit === 0 ? 'No' : 'Yes'}
+                        </li>
+                        <li className="list-group-item">
+                            <strong>Release Date</strong>: <Moment format="MM/DD/YYYY">{track.first_release_date}</Moment>
+                        </li>
+                    </ul>
+                </React.Fragment>
+            );
         }
   }
 }
