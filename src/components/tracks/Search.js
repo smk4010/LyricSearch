7b2325bcbd1f0277795eb6deabe6bcd04590e858
Scott Kuchinski <scottkuchinski@gmail.com>
2018-11-16 12:14:59 -0500

    search component scaffolding built

---
+++ src/components/tracks/Search.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import axios from 'axios';
+import { Consumer } from '../../context';
+
+class Search extends Component {
+    state = {
+        trackTitle: ''
+    };
+//arrow function allows you to get rid of bind(this), very handy for long forms...and for all things
+    onChange = (e) => {
+        this.setState({ [e.target.name]: e.target.value});
+    }
+
+  render() {
+    return (
+      <Consumer>
+        {value => {
+            return (
+                <div className="card card-body mb-4 p-4">
+                    <h1 className="display-4 text-center">
+                        <i className="fas fa-music"></i> Search For A Song
+                    </h1>
+                    <p className="lead text-center">Get the lyrics for any song</p>
+                    <form>
+                        <div className="form-group">
+                            <input 
+                                type="text" 
+                                className="form-control form-control-lg" 
+                                placeholder="Song title..." 
+                                name="trackTitle"
+                                value={this.state.trackTitle}
+                                onChange={this.onChange}
+                                />
+                        </div>
+                    </form>
+                </div>
+            );
+        }}
+      </Consumer>
+    )
+  }
+}
+
+export default Search;
